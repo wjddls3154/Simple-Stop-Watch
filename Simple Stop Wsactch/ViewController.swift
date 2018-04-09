4a5bc5d6368ab46761ff53cc75f9a795ad964e90
D7702_10 <d7702_10@D7702-10ui-iMac.local>
2018-04-09 16:03:18 +0900

    Simple Stop Watch

--- Simple Stop Wsactch/ViewController.swift
+++ Simple Stop Wsactch/ViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    var count = 0
+    var myTimer = Timer()
+    @IBOutlet weak var start: UIButton!
+    @IBOutlet weak var stop: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+      stop.isEnabled = false
+        
 }
 
+    @IBAction func btnStart(_ sender: Any) {
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in
+            self.updateTime()
+        })
+        start.isEnabled = false
+        stop.isEnabled = true
+    }
+    
+    @IBAction func btnStop(_ sender: Any) {
+        myTimer.invalidate()
+        start.isEnabled = true
+        stop.isEnabled = false
+    }
+    
+    @IBAction func btnReset(_ sender: Any) {
+        count = 0
+        timeLabel.text = "00:00:00"
+    }
+    
+    
+    func updateTime() {
+        count = count + 1
+        let min = count / 60 / 100
+        let sec = (count - (min * 60 * 100)) / 100
+        let msec = count - (min * 60 * 100) - (sec * 100)
+        timeLabel.text = String(format: "%02d:%02d:%02d",min,sec,msec)
+}
+}
